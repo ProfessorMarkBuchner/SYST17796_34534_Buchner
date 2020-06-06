d915ddbda8d72db2d628b33b523564317b81eadb
Athan360v2 <moralmau@sheridancollege.ca>
2020-06-05 23:30:36 -0400

    Initial comit

--- src/cardgameice1/CardGameICE1.java
+++ src/cardgameice1/CardGameICE1.java
@@ -1,4 +1,5 @@
-/*
+/*Name: Mauricio Morales
+ * SN: 991584677
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
