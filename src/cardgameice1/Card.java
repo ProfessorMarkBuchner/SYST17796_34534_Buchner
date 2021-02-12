d3a0ec4e2b32215d525adffabd8a262135e17610
Athan360v2 <moralmau@sheridancollege.ca>
2021-02-12 13:07:59 -0500

    Initial comit

--- src/cardgameice1/Card.java
+++ src/cardgameice1/Card.java
@@ -1,47 +1,36 @@
 /*Name: Mauricio Morales
  * SN: 991584677
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *
  */
 package cardgameice1;
 
-/**
- *
- * @author srinivsi
- 
- private variable then refactor it
- * - encapsulate fields (encapsulation principle) 
- getters/setters
- */
 public class Card {
     private String suits;//diamonds,clubs,spades, hearts
     private int value; // 1 to 13
-    public static final String[] SUITS={"diamonds","clubs","spades","hearts"};
+    public static final String[] SUITS={"Diamonds","Clubs","Spades","Hearts"};
     
     public String getSuits() {
         return suits;
     }
 
-    
     public void setSuits(String suits) {
         this.suits = suits;
     }
 
-    
     public int getValue() {
         return value;
     }
 
-    
     public void setValue(int value) {
         this.value = value;
     }
     
+    //random values between 1 and 13 
     public int randomValue(){
          value = (int)(Math.random()*13+1);
         return value;
     }
+    //random suit between Diamonds, Clubs, Spades, and  Heards
     public  String randomSuit() {
         suits = (Card.SUITS[(int)(Math.random()*3)]);
        return suits;
--- src/cardgameice1/CardTrick.java
+++ src/cardgameice1/CardTrick.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
  print 7 cards (values,suits) and takes input 
  then prints if matching card is found in an array
  */
-public class CardGameICE1{
+public class CardTrick{
 
     /**
      * @param args the command line arguments
@@ -29,7 +29,7 @@ public class CardGameICE1{
            c.setValue(c.randomValue());
            c.setSuits(c.randomSuit());
            magicHand[i]=c;//saving object in array
-           System.out.println(magicHand[i].getValue()+" "+magicHand[i].getSuits());
+           //System.out.println(magicHand[i].getValue()+" "+magicHand[i].getSuits());
         }
         
         //take input from user and compare with array\
@@ -43,14 +43,15 @@ public class CardGameICE1{
                 System.out.println("Number no valid!\t\t Try again");
             }
         }while(usernumber < 1 || usernumber > 13);
+        read.nextLine();//clean buffer
         
-        //validate suit user input
-        String suitUser="";//validate user input
+        //validate suit user input        
+        String suitUser="";
         int x=0;
         do{
             System.out.println("Please enter you suit1  (Diamonds, Clubs, Spades or Heards)");
             suitUser = read.nextLine();
-            if(suitUser.equals("diamonds")||suitUser.equals("clubs")||suitUser.equals("spades")||suitUser.equals("heards")){
+            if(suitUser.equals("Diamonds")||suitUser.equals("Clubs")||suitUser.equals("Spades")||suitUser.equals("Heards")){
                 x=1;
             }else{
                 System.out.println("Suit no valid! Try again");
@@ -58,12 +59,14 @@ public class CardGameICE1{
         }while(x != 1);
         
         //compare with array
-        for(int i=0;i<=13;i++){
+        for(int i=0;i<7;i++){
             if(usernumber==magicHand[i].getValue()&& suitUser.equals(magicHand[i].getSuits())){
-                
-                System.out.println("Your card is in the MagicHand!!!");
+                System.out.println("YEA!!! Your card is in the MagicHand!!!");
+                break;
             }else{
-                System.out.println("Your card is not in the MagicHand :(");
+                if(i==6){
+                    System.out.println("UPS!! Your card is not in the MagicHand :(");
+                }
             }
         }   
     }
