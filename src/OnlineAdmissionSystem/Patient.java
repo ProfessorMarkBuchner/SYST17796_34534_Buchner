da72686d66cee017c94c87dd3c5af778b3530ff5
aleks <aleks@192.168.2.49>
2021-02-12 15:44:16 -0500

    Added standard getter's and setters. It's clean accessing variables like that. Also, you can make all your variables private this way.

--- src/OnlineAdmissionSystem/Patient.java
+++ src/OnlineAdmissionSystem/Patient.java
@@ -11,15 +11,12 @@ import java.util.ArrayList;
 /**
  * This class represents students in our application
  *
- * @author Aleks Lim was here @ Sheridan College 2021 
+ * @author Aleks Lim was here @ Sheridan College 2021
  */
-
 //Class to model patients in an ER Admission System
 public class Patient {
-    
-    
-    //Fields:___________________________
 
+    //Fields:___________________________
     private String name;
     private LocalDate dob;
     private String family_doctor;
@@ -32,11 +29,9 @@ public class Patient {
     //Constructors:_________________________
     
     public Patient() {
-
+        
     }
 
-    
-    
     public Patient(
             String name,
             LocalDate dob,
@@ -51,18 +46,79 @@ public class Patient {
         this.dob = dob;
         this.family_doctor = family_doctor;
         this.medications = medications;
-        this.allergies = allergies; 
+        this.allergies = allergies;
         this.bandList = bandList;
-        this.admitDate = admitDate; 
-        
+        this.admitDate = admitDate;
+
     }//End M:*
 
+    //Getters:______________________________
+    
     public String getName() {
         return name;
     }//End G:*
 
+    public LocalDate getDob() {
+        return dob;
+    }//End G:*
+
+    public String get_Family_Doctor() {
+        return family_doctor;
+    }//End G:*
+
+    public ArrayList<String> getMedications() {
+        return medications;
+    }//End G:*
+
+    public ArrayList<String> getAllergies() {
+        return allergies;
+    }//End G:*
+
+    public ArrayList<WristBand> getBandList() {
+        return bandList;
+    }//End G:*
+
+    public WristBand getBasicBand() {
+        return basicBand;
+    }//End G:*
+
+    public LocalDate getAdmitDate() {
+        return admitDate;
+    }//End G:*
+
+    //Setters:__________________________
+    
+    
     public void setName(String name) {
         this.name = name;
+    }//End S:*
+
+    public void setDob() {
+        this.dob = dob;
+    }//End S:*
+
+    public void set_Family_Doctor() {
+        this.family_doctor = family_doctor;
+    }//End S:*
+
+    public void setMedications() {
+        this.medications = medications;
+    }//End S:*
+
+    public void setAllergies() {
+        this.allergies = allergies;
+    }//End S:*
+
+    public void setBandList() {
+        this.bandList = bandList;
+    }//End S:*
+
+    public void setBasicBand() {
+        this.basicBand = basicBand;
+    }//End S:*
+
+    public void setAdmitDate() {
+        this.admitDate = admitDate;
     }//End S:*
 
     //Methods
