d99687375dbd139f3ccebcbe458feebb37274e24
Eric <eric050117@outlook.com>
2021-02-14 02:19:06 -0500

    ICE 1 Starter Code
    Code for In class 1 by Enrico Ginocchi

---
+++ src/ICE1/src/sheridan/CardTrick.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sheridan;
+import java.util.*;
+
+
+/**
+ * A class that fills a magic hand of 7 cards with random Card Objects
+ * and then asks the user to pick a card and searches the array of cards
+ * for the match to the user's card. To be used as starting code in ICE 1
+ * @author dancye
+ */
+public class CardTrick {
+    
+    public static void main(String[] args)
+    {
+        Random rand = new Random();
+        Card[] magicHand = new Card[7];
+        
+        for (int i=0; i<magicHand.length; i++)
+        {
+            Card c = new Card();
+        }
+        
+        //insert code to ask the user for Card value and suit, create their card
+        // and search magicHand here
+        //Then report the result here
+    }
+    
+}
