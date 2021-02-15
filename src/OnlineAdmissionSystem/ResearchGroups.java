983d94f3d86259c7537dd563372f9a95508324c0
aleks <aleks@192.168.2.49>
2021-02-15 16:49:30 -0500

    Defined the method that ensures a patient is enrolled in 1 group at a time.

--- src/OnlineAdmissionSystem/ResearchGroups.java
+++ src/OnlineAdmissionSystem/ResearchGroups.java
@@ -16,21 +16,52 @@ public class ResearchGroups {
     
     //Fields
     
-    private ArrayList<Patient> groupList; 
+    private ArrayList<Patient> patientList; 
+    private ArrayList<ResearchGroups> groupList; 
+    private int duplicates; 
     
     //No Arg Constructor 
     public ResearchGroups(){
     }
     
+    //All Arg Constructor 
+    public ResearchGroups(ArrayList<Patient> patientList){
+        this.patientList = patientList; 
+    }//End C:*
+    
+    //Getters:
+    public ArrayList<Patient> getPatientList(){
+        return patientList;
+    }
+    
+    public ArrayList<ResearchGroups> getGroupList(){
+        return groupList;
+    }
+    
+    
+    //Setters:
+    public void setPatientList(ArrayList<Patient> newPL){
+        this.patientList = newPL;
+    }
+    
+    public void setGroupList(ArrayList<ResearchGroups> newGL){
+        this.groupList = newGL;
+    }
     
     //Methods
-    
-    public boolean maxGroups(){
-    boolean result = false;
-        if(this.groupList.size() > 10){
-            result = true; 
-        }//End I:*
-    return result; 
+    public boolean maxGroupEnrollment(Patient tPatient){
+        boolean result = false;
+        for(int i = 0; i < groupList.size(); i++){
+            for(int j = 0; j < groupList.get(i).getPatientList().size(); j++){
+                if(groupList.get(i).getPatientList().get(j).getId() == tPatient.getId()){
+                    duplicates += 1;
+                }//End I:*
+            }//End F1:*
+        }//End F2:*
+        return result;
     }//End M:*
+    
+    
+    
     
 }//End Class:*
