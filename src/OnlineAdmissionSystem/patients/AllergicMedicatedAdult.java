b43cd739f83d6f63365e900028a95055f3317da2
aleks <aleks@192.168.2.49>
2021-02-20 18:19:59 -0500

    Added medications to objects data.

--- src/OnlineAdmissionSystem/patients/AllergicMedicatedAdult.java
+++ src/OnlineAdmissionSystem/patients/AllergicMedicatedAdult.java
@@ -5,12 +5,45 @@
  */
 package OnlineAdmissionSystem.patients;
 
+//Imports:______________________
+
+import OnlineAdmissionSystem.wristbands.WristBand;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 /**
  *
- * @author aleks
+ * @author AllyCat13 @ Sheridan College 2021
  */
-public class AllergicMedicatedAdult{
+public class AllergicMedicatedAdult extends AllergicAdult{
     
+    protected ArrayList<String> medications; 
     
+    public AllergicMedicatedAdult(
+             int id,
+             String name,
+             LocalDate dob,
+             String family_doctor,
+             String medicalProblem,
+             ArrayList<WristBand> bandList,
+             WristBand basicBand,
+             LocalDate admitDate,
+             ArrayList<String> allergies,
+             ArrayList<String> medications
+            ){
+    
+        super(
+                id,
+                name, 
+                dob, 
+                family_doctor, 
+                medicalProblem,
+                bandList, 
+                basicBand, 
+                admitDate,
+                allergies
+        );//End SU:*
+        this.medications = medications; 
+    }//End C:*
     
 }//End Class:__________________
