973e496596e4f7f818a716e3060de20696fb5c15
aleks <aleks@192.168.2.49>
2021-02-20 17:04:01 -0500

    further refining patient types.

---
+++ src/OnlineAdmissionSystem/patients/MedicatedAdult.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OnlineAdmissionSystem.patients;
+
+/**
+ *
+ * @author AllyCat @ Sheridan College 2021
+ */
+public class MedicatedAdult extends AdultPatient{
+    
+}//End Class:*
