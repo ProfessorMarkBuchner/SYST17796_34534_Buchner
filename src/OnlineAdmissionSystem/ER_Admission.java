87fd7de8a42fb793647ca2c67be1a959db0f7561
aleks <aleks@192.168.2.49>
2021-02-20 14:43:51 -0500

    Created control structure to build patient based on output of er admissions check age method.

--- src/OnlineAdmissionSystem/ER_Admission.java
+++ src/OnlineAdmissionSystem/ER_Admission.java
@@ -7,17 +7,32 @@ package OnlineAdmissionSystem;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Scanner;
 
 /**
  *
- * @author Aleks Lim @ Sheridan College 2021
+ * @author AllyCat @ Sheridan College 2021
+ * Can't do UML until the programs complete.
  */
 public class ER_Admission {
     
     //This var is just for usage and doesn't change 
     private final UserInput input;
+    private Patient patient;
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
     
     //Fields:__________________________________________
+
+    public UserInput getInput() {
+        return input;
+    }
     
     
     //Constructors:_____________________________________
@@ -36,9 +51,23 @@ public class ER_Admission {
     
     public static void main (String[] args){
         
+        ER_Admission er = new ER_Admission();
+        
+        
         //Patient enters Er and provides dob 
-        //System.out.println("Can I have your name and date of birth please");
-        //System.out.println(LocalDate.now());
+        //Format is year-month-day 
+        //A: Get DOB because patient type depends on it. 
+        System.out.println("Can I have your date of birth please like year-month-day");
+        String sDob = er.getInput().promptStringUser(new Scanner(System.in));
+        //B:
+        LocalDate lDob = LocalDate.parse(sDob);
+        boolean checkAge = er.isAdult(lDob);
+        if(checkAge == true){
+            er.setPatient(new AdultPatient());
+        }//End I:*
+        else {
+        er.setPatient(new ChildPatient());
+        }//End E:*
         
     }//End M:*
     
