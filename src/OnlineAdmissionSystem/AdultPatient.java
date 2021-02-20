71cddcbdfaafb2f2a886a66e520151be3c410edf
aleks <aleks@192.168.2.49>
2021-02-20 12:49:07 -0500

    Got rid of check age because I think it should be in ER admission class.

---
+++ src/OnlineAdmissionSystem/AdultPatient.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OnlineAdmissionSystem;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Aleks Lim @ Sheridan College 2021
+ */
+public class AdultPatient extends Patient{
+    
+    public AdultPatient(){
+        
+    }//End C:*
+    
+    
+    public AdultPatient(
+             int id,
+             String name,
+             LocalDate dob,
+             String family_doctor,
+             ArrayList<String> medications,
+             ArrayList<String> allergies,
+             ArrayList<WristBand> bandList,
+             WristBand basicBand,
+             LocalDate admitDate
+    ){
+        super(
+                id,
+                name, 
+                dob, 
+                family_doctor, 
+                medications, 
+                allergies, 
+                bandList, 
+                basicBand, 
+                admitDate
+        );
+    }
+   
+}//End Class:*
