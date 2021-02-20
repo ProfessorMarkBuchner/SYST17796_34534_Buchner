9d7cf15df4ac5a561c52c8c74fd0c4ca3162bf35
aleks <aleks@192.168.2.49>
2021-02-20 16:21:37 -0500

    Created folders for organization because classes growing wildly.

---
+++ src/OnlineAdmissionSystem/wristbands/ChildrensWristBand.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OnlineAdmissionSystem.wristbands;
+
+import OnlineAdmissionSystem.patients.ChildPatient;
+
+/**
+ *
+ * @author aleks
+ */
+public class ChildrensWristBand extends WristBand{
+    
+   
+    
+    public ChildrensWristBand(){
+    
+    }//End C:*
+    
+    public ChildrensWristBand(
+                              Long barcode,
+                              ChildPatient patient
+                              ){
+        super(
+              barcode, 
+              patient
+              );
+    
+    }//End C:*
+    
+    @Override 
+    public void printBand(){
+    super.printBand();
+    if(patient instanceof ChildPatient){
+        ChildPatient converted = (ChildPatient) patient;
+        System.out.println(converted.getParent());
+    }//End I:*
+        
+    }//End M:*
+    
+}//End Class:*
