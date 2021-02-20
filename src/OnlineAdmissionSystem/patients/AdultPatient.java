a96b8dbaa5879c7a169cc7becbeb27b3e56f8752
aleks <aleks@192.168.2.49>
2021-02-20 16:20:57 -0500

    Created folders for organization because classes growing wildly.

---
+++ src/OnlineAdmissionSystem/patients/AdultPatient.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OnlineAdmissionSystem.patients;
+
+import OnlineAdmissionSystem.wristbands.WristBand;
+import OnlineAdmissionSystem.patients.Patient;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Aleks Lim @ Sheridan College 2021
+ */
+public class AdultPatient extends Patient{
+    
+    public AdultPatient(){
+        
+    }//End C:*
+    
+    
+    public AdultPatient(
+             int id,
+             String name,
+             LocalDate dob,
+             String family_doctor,
+             String medicalProblem,
+             ArrayList<String> medications,
+             ArrayList<WristBand> bandList,
+             WristBand basicBand,
+             LocalDate admitDate
+    ){
+        super(
+                id,
+                name, 
+                dob, 
+                family_doctor, 
+                medicalProblem,
+                medications, 
+                bandList, 
+                basicBand, 
+                admitDate
+        );
+    }
+   
+}//End Class:*
