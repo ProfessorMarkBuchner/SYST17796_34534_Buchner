a60ac12b2b645ec6706e778c327ce5632a97e346
aleks <aleks@192.168.2.49>
2021-02-20 17:04:05 -0500

    further refining patient types.

---
+++ src/OnlineAdmissionSystem/patients/MedicatedChild.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OnlineAdmissionSystem.patients;
+
+/**
+ *
+ * @author AllyCat @ Sheridan College 2021
+ */
+public class MedicatedChild extends ChildPatient{
+    
+    
+}//End Class:*
