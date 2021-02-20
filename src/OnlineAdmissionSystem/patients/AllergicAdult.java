5fc0d6b6124c6565c88cb2c1fa39e47724e5882a
aleks <aleks@192.168.2.49>
2021-02-20 16:38:51 -0500

    further refining patient types.

---
+++ src/OnlineAdmissionSystem/patients/AllergicAdult.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OnlineAdmissionSystem.patients;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author AllyCat @ Sheridan College 2021
+ */
+public class AllergicAdult extends AdultPatient{
+    
+    protected ArrayList<String> allergies;
+    
+   
+    
+}//End Class:*
