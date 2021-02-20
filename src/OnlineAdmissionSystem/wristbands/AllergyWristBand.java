4f2f939a7222570488a2fc80e5f9a52dd799027a
aleks <aleks@192.168.2.49>
2021-02-20 16:21:33 -0500

    Created folders for organization because classes growing wildly.

---
+++ src/OnlineAdmissionSystem/wristbands/AllergyWristBand.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OnlineAdmissionSystem.wristbands;
+
+import OnlineAdmissionSystem.patients.Patient;
+
+/**
+ *
+ * @author Aleks Lim @Sheridan College 2021
+ */
+public class AllergyWristBand extends WristBand{
+    
+    //Constrcutors:_________________________________
+    
+    public AllergyWristBand(){
+    
+    }
+    
+    public AllergyWristBand(
+                             Long barcode,
+                             Patient patient
+                            ){
+        super(
+                barcode,
+                patient
+              );
+    }//End C:*
+    
+    @Override
+    public void printBand(){
+    super.printBand();
+        //System.out.println(patient.getAllergies());
+    }
+    
+}//End Class:*
