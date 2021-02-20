7321ef43e4441105f7f5729255128cd3de7c1464
aleks <aleks@192.168.2.49>
2021-02-20 16:21:17 -0500

    Created folders for organization because classes growing wildly.

---
+++ src/OnlineAdmissionSystem/patients/ChildPatient.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OnlineAdmissionSystem.patients;
+
+import OnlineAdmissionSystem.wristbands.WristBand;
+import OnlineAdmissionSystem.patients.Patient;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Aleks Lim was here @ Sheridan College 2021
+ */
+
+//Class to model children as different type of patient 
+public class ChildPatient extends Patient{
+    
+    //Fields:____________________________
+    
+    private String parent;
+    
+    //Constructors:_______________________________
+    
+    public ChildPatient(
+            int id,
+             String parent,
+             String name,
+             LocalDate dob,
+             String family_doctor,
+             String medicalProblem,
+             ArrayList<String> medications,
+             ArrayList<String> allergies,
+             ArrayList<WristBand> bandList,
+             WristBand basicBand,
+             LocalDate admitDate
+    ){
+        super(
+                id,
+                name, 
+                dob, 
+                family_doctor, 
+                medicalProblem,
+                medications, 
+                bandList, 
+                basicBand, 
+                admitDate
+        );
+        this.parent = parent; 
+    }//End C:*
+    
+    //No Arg Constructor 
+    public ChildPatient(){
+    
+    }//End C:*
+    
+    
+    //Getters:___________________________
+    
+    public String getParent(){return parent;}
+    
+    //Setters:______________________________
+    
+    public void setParent(String newParent){ this.parent = newParent;}
+    
+    
+    
+    
+}//End Class:________________________
