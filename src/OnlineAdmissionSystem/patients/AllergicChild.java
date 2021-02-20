4b50240ec89cd91c07f14757217c1fb1dbb2fa3c
aleks <aleks@192.168.2.49>
2021-02-20 16:21:02 -0500

    Created folders for organization because classes growing wildly.

---
+++ src/OnlineAdmissionSystem/patients/AllergicChild.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OnlineAdmissionSystem.patients;
+
+import OnlineAdmissionSystem.patients.ChildPatient;
+
+/**
+ *
+ * @author aleks
+ */
+public class AllergicChild extends ChildPatient{
+    
+}
