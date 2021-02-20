8725c273c730ab454b60ce6a6ad4b8a69ac0c3bb
aleks <aleks@192.168.2.49>
2021-02-20 17:59:23 -0500

    further refining patient types.

--- src/OnlineAdmissionSystem/patients/MedicatedChild.java
+++ src/OnlineAdmissionSystem/patients/MedicatedChild.java
@@ -5,11 +5,49 @@
  */
 package OnlineAdmissionSystem.patients;
 
+//Imports:____________________________
+
+import OnlineAdmissionSystem.wristbands.WristBand;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 /**
  *
  * @author AllyCat @ Sheridan College 2021
  */
 public class MedicatedChild extends ChildPatient{
     
+    protected ArrayList<String> medications;
+    
+    public MedicatedChild(){
+        
+    }//End C:*
+    
+    public MedicatedChild(
+             int id,
+             String parent,
+             String name,
+             LocalDate dob,
+             String family_doctor,
+             String medicalProblem,
+             ArrayList<WristBand> bandList,
+             WristBand basicBand,
+             LocalDate admitDate,
+             ArrayList<String> medications
+            ){
+        super(
+                id,
+                name, 
+                parent,
+                dob, 
+                family_doctor, 
+                medicalProblem,
+                bandList, 
+                basicBand, 
+                admitDate
+        );
+        this.medications = medications; 
+    
+    }//End C:*
     
 }//End Class:*
