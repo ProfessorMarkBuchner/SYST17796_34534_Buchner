56ef93e883f9bc76f454ba3181ea9c20a95ef57c
aleks <aleks@192.168.2.49>
2021-02-20 20:31:26 -0500

    The entry point changed.

--- src/OnlineAdmissionSystem/ER_Admission/ER_Admission.java
+++ src/OnlineAdmissionSystem/ER_Admission/ER_Admission.java
@@ -38,10 +38,7 @@ public class ER_Admission {
     private WristBand wristBand;
     private LocalDate dob;
     
-    
-  
-
-    
+   
     //Getters & Setters:____________________________
     
     
@@ -87,37 +84,10 @@ public class ER_Admission {
     public static void main (String[] args){
         
         ER_Admission er = new ER_Admission();
+        IntakeForm form = new IntakeForm();
         
-        
-        //Patient enters Er and provides dob 
-        //Format is year-month-day 
-        
-        //A: Get DOB because patient type depends on it. 
-        System.out.println("Can I have your date of birth please like year-month-day");
-        String sDob = er.getInput().promptStringUser(new Scanner(System.in));
-        LocalDate lDob = LocalDate.parse(sDob);
-        
-        //B: Evaluate 
-        boolean checkAge = er.isAdult(lDob);
-        if(checkAge == true){
-            er.setPatient(new AdultPatient());
-            AdultPatient converted = (AdultPatient)er.getPatient();
-            converted.setDob(lDob);
-            converted.setAdmitDate(LocalDate.now());
-            er.setPatient(converted);
-        }//End I:*
-        else {
-        er.setPatient(new ChildPatient());
-            ChildPatient converted = (ChildPatient)er.getPatient();
-            System.out.println("Can I have your parents name plase?");
-            converted.setParent(er.getInput().promptStringUser(new Scanner(System.in)));
-            converted.setDob(lDob);
-            converted.setAdmitDate(LocalDate.now());
-            er.setPatient(converted);       
-        }//End E:*
-        
-        //C:
-        er.askForItAll();
+       
+       
         
     }//End M:*
     
@@ -131,40 +101,6 @@ public class ER_Admission {
         return result;
     }//End M:*
     
-    /*
-    Need to know if patient has allergies because wrist band type
-    is dependent on this.*/
-    
-    public boolean checkAllergies(){
-        boolean result = false;
-        String answer = this.getInput().promptStringUser(new Scanner(System.in));
-        if(answer.equals("yes")){
-               result = true;
-        }//End I:*
-        return result;
-    }//End M:*
-    
-    
-    public void askForItAll(){
-        /*
-    protected int id;
-    protected String name;
-    protected LocalDate dob;
-    protected String family_doctor;
-    private String medicalProblem;
-    protected ArrayList<String> medications;
-    protected ArrayList<String> allergies;
-    protected ArrayList<WristBand> bandList;
-    protected WristBand basicBand;
-    protected LocalDate admitDate;
-        */
-        
-        this.patient.setName(input.promptStringUser(new Scanner(System.in)));
-        this.patient.setFamily_doctor(input.promptStringUser(new Scanner(System.in)));
-        this.patient.setId(this.patient.getNumberOfPatients() + 1);
-        this.patient.setMedicalProblem(input.promptStringUser(new Scanner(System.in)));
-    }
-    
-    
+
     
 }//End Class:*
