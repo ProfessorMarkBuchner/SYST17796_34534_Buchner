a729c229277f9f6302e9c221985732c9f442da1e
aleks <aleks@192.168.2.49>
2021-02-20 23:03:16 -0500

    Extended print method per type of patients needs.

--- src/OnlineAdmissionSystem/wristbands/AllergicAdultBand.java
+++ src/OnlineAdmissionSystem/wristbands/AllergicAdultBand.java
@@ -5,14 +5,33 @@
  */
 package OnlineAdmissionSystem.wristbands;
 
+import OnlineAdmissionSystem.patients.AllergicAdult;
+import OnlineAdmissionSystem.patients.Patient;
+
 /**
  *
  * @author AllyCat13 Sheridan High 2021 
+ * Generic programming facilitates polymorphism.
  */
-public class AllergicAdultBand {
-
-    
+public class AllergicAdultBand extends AdultBand{
+        
     public AllergicAdultBand() {
+        
+    }//End C:*
+    
+    public AllergicAdultBand(Long barcode, Patient patient) {
+        super(barcode, new AllergicAdult());
+    }//End C:*
+    
+    @Override
+    public void print(){
+        super.print();
+        if(this.patient instanceof AllergicAdult){
+            AllergicAdult converted = (AllergicAdult)this.patient;
+            for(int i = 0; i < converted.getAllergies().size(); i++){
+                System.out.println(converted.getAllergies().get(i));
+            }//End F:*
+        }//End I:*
         
     }//End C:*
     
