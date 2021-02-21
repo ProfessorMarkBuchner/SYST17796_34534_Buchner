63551d963888ad5cef40c1f1ca368da866587445
aleks <aleks@192.168.2.49>
2021-02-20 23:03:40 -0500

    Extended print method per type of patients needs.

---
+++ src/OnlineAdmissionSystem/wristbands/MedicatedAdultBand.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OnlineAdmissionSystem.wristbands;
+
+import OnlineAdmissionSystem.patients.MedicatedAdult;
+import OnlineAdmissionSystem.patients.Patient;
+
+/**
+ *
+ * @author AllyCat13 Sheridan High 2021 
+ */
+public class MedicatedAdultBand extends AdultBand{
+
+    public MedicatedAdultBand() {
+        
+    }//End C:*
+    
+    public MedicatedAdultBand(Long barcode, Patient patient) {
+        super(barcode, new MedicatedAdult());
+    }//End C:*
+    
+    //Methods:________________________
+    
+    @Override 
+    public void print(){
+        super.print();
+        if(this.patient instanceof MedicatedAdult){
+            MedicatedAdult converted = (MedicatedAdult)this.patient;
+            for(int i = 0; i < converted.getMedications().size(); i++){
+                System.out.println(converted.getMedications().get(i));
+            }//End F:*
+        }//End I:*
+        
+    }//End C:*
+    
+}//End Class:____________________________
