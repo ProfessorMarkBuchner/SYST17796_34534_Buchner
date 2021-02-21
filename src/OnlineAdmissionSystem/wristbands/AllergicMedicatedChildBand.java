1ffe32bb7de1b5d91727621e370a902e2c82a153
aleks <aleks@192.168.2.49>
2021-02-20 21:43:02 -0500

    Getting bands classes ready for addition of print method.

---
+++ src/OnlineAdmissionSystem/wristbands/AllergicMedicatedChildBand.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OnlineAdmissionSystem.wristbands;
+
+/**
+ *
+ * @author AllyCat13 Sheridan High 2021
+ */
+
+public class AllergicMedicatedChildBand extends WristBand{
+
+    public AllergicMedicatedChildBand() {
+        
+    }//End C:*
+    
+    
+}//End Class:_________________________
