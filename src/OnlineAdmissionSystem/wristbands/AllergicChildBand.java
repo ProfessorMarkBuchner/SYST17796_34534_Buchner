bced144e52e020103d904e73199113ba7764d173
aleks <aleks@192.168.2.49>
2021-02-20 23:03:20 -0500

    Extended print method per type of patients needs.

--- src/OnlineAdmissionSystem/wristbands/AllergicChildBand.java
+++ src/OnlineAdmissionSystem/wristbands/AllergicChildBand.java
@@ -5,16 +5,16 @@
  */
 package OnlineAdmissionSystem.wristbands;
 
+import OnlineAdmissionSystem.patients.AllergicChild;
 import OnlineAdmissionSystem.patients.Patient;
 
 /**
  *
- * @author Aleks Lim @Sheridan College 2021
+ * @author AllyCat13 @Sheridan High 2021
  */
-public class AllergicChildBand extends WristBand{
+public class AllergicChildBand extends ChildBand{
     
-   
-            
+
     //Constrcutors:_________________________________
     
     public AllergicChildBand(){
@@ -27,14 +27,21 @@ public class AllergicChildBand extends WristBand{
                             ){
         super(
                 barcode,
-                patient
+                new AllergicChild()
               );
     }//End C:*
     
+    //Everything professor communicates is by way of analogy.
     @Override
     public void print(){
     super.print();
-        //System.out.println(patient.getAllergies());
-    }
+        if(this.patient instanceof AllergicChild){
+            AllergicChild converted = (AllergicChild)this.patient;
+            System.out.println(converted.getParent());
+            for(int i = 0; i < converted.getAllergies().size(); i++){
+                System.out.println(converted.getAllergies().get(i));
+            }//End F:*
+        }//End I:*
+    }//End M:*
     
 }//End Class:*
