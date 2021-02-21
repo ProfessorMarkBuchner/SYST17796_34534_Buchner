932d7a65a73d158728337c34c48a0eea59ef00db
aleks <aleks@192.168.2.49>
2021-02-20 21:43:11 -0500

    Getting bands classes ready for addition of print method.

---
+++ src/OnlineAdmissionSystem/wristbands/MedicationBand.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OnlineAdmissionSystem.wristbands;
+
+/**
+ *
+ * @author AllyCat13 Sheridan High 2021 
+ */
+public class MedicationBand extends WristBand{
+
+    public MedicationBand() {
+        
+    }//End C:*
+    
+    
+    
+}//End Class:______________________
