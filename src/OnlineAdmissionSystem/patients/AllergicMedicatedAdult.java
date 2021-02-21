2ee8cc68e571c825d341bd85c89092572064a6be
aleks <aleks@192.168.2.49>
2021-02-20 23:09:08 -0500

    removed print from patients because wrist bands take care of that.

--- src/OnlineAdmissionSystem/patients/AllergicMedicatedAdult.java
+++ src/OnlineAdmissionSystem/patients/AllergicMedicatedAdult.java
@@ -65,13 +65,7 @@ public class AllergicMedicatedAdult extends AllergicAdult{
     
     //Methods:______________________________________________________
     
-    @Override
-    public void print(){
-        super.print();
-        for(int i = 0; i < medications.size(); i++){
-            System.out.println(medications.get(i));
-        }//End F:*
-    }//End M:*
+ 
     
     
 }//End Class:_________________________________________________________
