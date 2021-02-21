730a4d4cf15fd0cc9d7bca5c424ae40a4253d30d
aleks <aleks@192.168.2.49>
2021-02-20 23:03:44 -0500

    Extended print method per type of patients needs.

---
+++ src/OnlineAdmissionSystem/wristbands/MedicationChildBand.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OnlineAdmissionSystem.wristbands;
+
+/**
+ *
+ * @author AllyCat13 Sheridan High 2021 
+ */
+public class MedicationChildBand extends WristBand{
+
+    public MedicationChildBand() {
+        
+    }//End C:*
+    
+    
+    
+}//End Class:______________________
